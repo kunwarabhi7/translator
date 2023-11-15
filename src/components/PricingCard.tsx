3f8dc2f6a90f5999344a1a7d5a81d877bbf22ab9
Abhishek <abhishekparmar162@gmail.com>
2023-11-15 12:51:25 +0530

    style the button

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -83,7 +83,12 @@ const PricingCard = ({ redirect }: { redirect: boolean }) => {
               </ul>
             </div>
             {redirect ? (
-              <Link href="/register">Get Started Today</Link>
+              <Link
+                className="bg-cyan-500 block mt-8 text-center text-white px-3.5 text-sm font-semibold leading-6 shadow-sm hover:bg-cyan-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 cursor-pointer py-2 rounded-md"
+                href="/register"
+              >
+                Get Started Today
+              </Link>
             ) : (
               tier.id && <CheckOutButton />
             )}
