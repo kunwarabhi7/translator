c631b81324a303de9c7a954b38050c34a2ed196d
Abhishek <abhishekparmar162@gmail.com>
2023-11-15 13:41:56 +0530

    added register button

--- src/components/CheckOutButton.tsx
+++ src/components/CheckOutButton.tsx
@@ -1,5 +1,9 @@
 const CheckOutButton = () => {
-  return <div>CheckOutButton</div>;
+  return (
+    <button className="bg-cyan-500 block mt-8 text-center text-white px-3.5 text-sm font-semibold leading-6 shadow-sm hover:bg-cyan-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 cursor-pointer py-2 rounded-md">
+      Sign up
+    </button>
+  );
 };
 
 export default CheckOutButton;
